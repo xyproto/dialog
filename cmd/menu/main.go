5c72b2c8246c63e4461f8571cdf7848fcc81e754
Alexander F. Rødseth <rodseth@gmail.com>
2020-03-10 11:59:42 +0100

    Fix the menu example

--- cmd/menu/main.go
+++ cmd/menu/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	d := dialog.New(80, 24)
-	answer := d.Menu("Choose your number", 6, map[string]string{"2": "More than one", "7": "A quality choice", "13": "A controversial option"})
+	answer, err := d.Menu("Choose your number", 6, map[string]string{"2": "More than one", "7": "A quality choice", "13": "A controversial option"})
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Your number: " + answer)
 }
